99e6ab77fa532f1bbbc0523040869876f0b160c8
Nandana <nandana.srinivasan@dal.ca>
2019-02-06 15:24:06 -0400

    All tasks complete

--- part_1.py
+++ part_1.py
@@ -1,7 +1,7 @@
 #PART - 1
 
 import re
-
+'''
 def list_ifname_ip(fobj):
 	reg_pat = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 	name_list = []
@@ -39,10 +39,10 @@ for line in filein:
 	#line = line.strip()
 	#word = line.split()
 
-	'''for i in word:
+	for i in word:
 		if(reg.match(i)):
 			if(('192' in i) or ('172'in i)
-				ip_addr.append(i)'''
+				ip_addr.append(i)
 	line = line.replace('192','10')
 	line = line.replace('172','10')
 	line = line.replace('255.255.0.0','255.0.0.0')
@@ -53,6 +53,29 @@ final_file = open("config_file",'r')
 for l in final_file:
 	print(l)
 #print(ip_addr)
+
+'''
+
+# TASK 3
+
+file_in = open("running-config.cfg")
+transit_list = []
+fw_list = []
+global_list = []
+for my_line in file_in:
+	my_line = my_line.strip()
+	if("transit_access_in" in my_line):
+		transit_list.append(my_line)
+	elif("global_access" in my_line):
+		global_list.append(my_line)
+	elif("fw-management_access_in" in my_line):
+		fw_list.append(my_line)
+
+print(transit_list,end = "\n")
+print(fw_list,end = "\n")
+print(global_list,end = "\n")
+
+
 
 
 
